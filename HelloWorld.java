4f7fefc9fdb5bbebdf1d093aa043b8bf3d011d31
MIL89 <30861576+MIL89@users.noreply.github.com>
2021-03-22 18:31:20 +0530

    Trail12

--- HelloWorld.java
+++ HelloWorld.java
@@ -7,5 +7,7 @@ class HelloWorld{
   public static void add(int num1,int num2){
       System.out.println(num1+num2);
   }
-
+public static void diff(int num1,int num2){
+      System.out.println(num1-num2);
+  }
 }
